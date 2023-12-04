566957ae1f632aadf6cc7de04996eab1ebd490e7
avof1ow <avof1ow4@gmail.com>
2023-12-04 18:20:03 +0000

    Добавлен скрипт приниающий ввод

---
+++ Scripts/inputDate.py
@@ -0,0 +1,26 @@
+import datetime
+
+import pandas as pd
+
+
+def main(year: int, month: int, day: int) -> tuple[str, int]:
+    """_summary_
+        Принимаются день, месяц и год
+        возвращается курс по заданной дате
+    Args:
+        year (int): год нужного курса
+        month (int): месяц нужного курса
+        day (int): день нужного курса
+
+    Returns:
+        tuple[str, int]: возвращает кортеж с датой и курсом рубля
+    """
+    df = pd.read_csv("CSV/dollar_course.csv", usecols=["rouble", "date"])
+    date = datetime.date(year, month, day)
+    rouble = df[df["date"] == str(date)]["rouble"].values[0]
+    return (str(date), rouble)
+
+
+if __name__ == "__main__":
+    print(main
+(2022, 12, 27))
